cf53b8f34cf58b4e2c0cc2c347fd8c0570b4d021
codefan <codefan@sina.com>
2019-07-02 15:10:42 +0800

    无法获取 spring boot 2.2.0.M3

---
+++ centit-cas-boot-demo/src/main/java/com/centit/cas/boot/demo/CentitCasBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.centit.cas.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentitCasBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentitCasBootDemoApplication.class, args);
+    }
+
+}
---
+++ centit-cas-boot-demo/src/test/java/com/centit/cas/boot/demo/CentitCasBootDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.centit.cas.boot.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CentitCasBootDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
